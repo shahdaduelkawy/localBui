9b0353c83ac1d136b6e6512b91c0824296f0c15a
shahd abduelkay <shahdabduelqawy@gmail.com>
2023-12-07 23:59:06 +0200

    a

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,33 +1,32 @@
 const nodemailer = require('nodemailer');
-
 const sendEmail = async (options) => {
-  // Create a transporter with your SMTP server settings
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
-    auth: {
-      user: 'yusefmuhmed6766@gmail.com',
-      pass: 'vfhr wfqi cvnb zdoe',
-    },
-  });
+// Create a transporter with your SMTP server settings
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: 'yusefmuhmed6766@gmail.com',
+    pass: 'vfhr wfqi cvnb zdoe',
+  },
+ 
+});
 
-  // Email content/options
-  const mailOptions = {
-    from: 'local App <localbusiness318@gmail.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
-
-  try {
-    // Send email
-    const info = await transporter.sendMail(mailOptions);
-    console.log('Email sent successfully!', info);
-    return info
-  } catch (error) {
-    console.error('Error occurred:', error);
-  }
+// Email content/options
+const mailOptions = {
+  from: 'local App <localbusiness318@gmail.com>',
+  to: options.email,
+  subject: options.subject,
+  text: options.message,
 };
 
+// Send email
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    console.error('Error occurred:', error);
+  } else {
+    console.log('Email sent successfully!');
+  }
+});
+};
 module.exports = sendEmail;
