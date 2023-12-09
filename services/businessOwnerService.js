b8f03057615e63fdd917da6803541fbff0be5a31
yusefmuhmed <yusefmuhmedd6766@gmail.com>
2023-12-09 22:39:03 +0200

    Upload attachment is done

--- services/businessOwnerService.js
+++ services/businessOwnerService.js
@@ -1,30 +1,46 @@
 const BusinessOwner = require("../models/businessOwnerModel");
 
 const BusinessOwnerService = {
-  async getUserBusiness(ownerID) {
-    try {
-      const businessData = await BusinessOwner.findOne({ userId: ownerID });
+    async getUserBusiness(ownerID) {
+        try {
+            const businessData = await BusinessOwner.findOne({
+                userId: ownerID
+            });
 
-      return businessData;
-    } catch (error) {
-      console.error("Error retrieving user business:", error);
-      return null;
+            return businessData;
+        } catch (error) {
+            console.error("Error retrieving user business:", error);
+            return null;
+        }
+    },
+
+    async updateUserBusiness(ownerID, updatedUserData) {
+        try {
+            const updateResult = await BusinessOwner.updateOne({
+                    userId: ownerID
+                },
+                updatedUserData
+            );
+
+            return updateResult;
+        } catch (error) {
+            console.error("Error updating user business:", error);
+            return null;
+        }
+    },
+
+    async uploadImage(ownerID, file) {
+        try {
+            const updateResult = await BusinessOwner.updateOne({
+                userId: ownerID
+            }, {
+                attachment: file.path
+            });
+            return updateResult
+        } catch (error) {
+            return error.message
+        }
     }
-  },
-
-  async updateUserBusiness(ownerID,updatedUserData) {
-    try {
-      const updateResult = await BusinessOwner.updateOne(
-        { userId: ownerID },
-        updatedUserData
-      );
-
-      return updateResult;
-    } catch (error) {
-      console.error("Error updating user business:", error);
-      return null;
-    }
-  },
 };
 
 module.exports = BusinessOwnerService;
